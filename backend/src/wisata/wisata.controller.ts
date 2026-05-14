67f19533374b37002dde3ecd7323eb486ef33333
projectgh123 <muhammad_aira@teknokrat.ac.id>
2026-05-14 13:37:28 +0700

    wisata controll

---
+++ backend/src/wisata/wisata.controller.ts
@@ -0,0 +1,47 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+
+import { WisataService } from './wisata.service';
+import { CreateWisataDto } from './dto/create-wisata.dto';
+import { UpdateWisataDto } from './dto/update-wisata.dto';
+
+@Controller('wisata')
+export class WisataController {
+
+  constructor(private wisataService: WisataService) {}
+
+  @Get()
+  findAll() {
+    return this.wisataService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.wisataService.findOne(Number(id));
+  }
+
+  @Post()
+  create(@Body() body: CreateWisataDto) {
+    return this.wisataService.create(body);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() body: UpdateWisataDto,
+  ) {
+    return this.wisataService.update(Number(id), body);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.wisataService.remove(Number(id));
+  }
+}
