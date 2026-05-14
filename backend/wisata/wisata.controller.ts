0c99dd62bfca3ea0a2189b7dac94434ace106cdf
projectgh123 <muhammad_aira@teknokrat.ac.id>
2026-05-14 11:32:46 +0700

    delete wisata

--- backend/wisata/wisata.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('wisata')
-export class WisataController {}
--- backend/wisata/wisata.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { WisataController } from './wisata.controller';
-import { WisataService } from './wisata.service';
-
-@Module({
-  controllers: [WisataController],
-  providers: [WisataService]
-})
-export class WisataModule {}
