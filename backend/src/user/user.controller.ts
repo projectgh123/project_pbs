d03cf45c4f61766fef4dd6c2cc33f59d25fb6335
projectgh123 <muhammad_aira@teknokrat.ac.id>
2026-05-14 14:04:07 +0700

    user controll

---
+++ backend/src/user/user.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get } from '@nestjs/common';
+import { UserService } from './user.service';
+
+@Controller('user')
+export class UserController {
+
+  constructor(private userService: UserService) {}
+
+  @Get()
+  getUsers() {
+    return this.userService.findAll();
+  }
+}
