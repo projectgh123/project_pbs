408591dc376bae797039fcfdaf02ef4b89514ff4
projectgh123 <muhammad_aira@teknokrat.ac.id>
2026-05-14 11:31:31 +0700

    delete gateway

--- backend/gateway/src/auth.client.ts
+++
@@ -1,9 +0,0 @@
-import { ClientProxyFactory, Transport } from '@nestjs/microservices';
-
-export const AuthClient = ClientProxyFactory.create({
-  transport: Transport.TCP,
-  options: {
-    host: 'localhost',
-    port: 4001,
-  },
-});
