25704904bd4ece2b0752723b9121267694abd3ea
projectgh123 <muhammad_aira@teknokrat.ac.id>
2026-05-15 11:03:13 +0700

    update app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
+import { AuthModule } from './auth/auth.module';
+import { WisataModule } from './wisata/wisata.module';
+import { ReviewModule } from './review/review.module';
+import { UserModule } from './user/user.module';
+import { UploadModule } from './upload/upload.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    AuthModule,
+    WisataModule,
+    ReviewModule,
+    UserModule,
+    UploadModule,
+    PrismaModule,
+  ],
 })
 export class AppModule {}
