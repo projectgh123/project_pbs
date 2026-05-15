c64fc19a62e5e11360eb2f7a27e641e79533e83c
SyaifulArief <syaiful_arief@teknokrat.ac.id>
2026-05-15 21:21:56 +0700

    Memperbaiki Eror AppNacigator

--- user/src/navigation/AppNavigator.tsx
+++ user/src/navigation/AppNavigator.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+// @ts-ignore: modul yang diimpor menggunakan TSX, tetapi JSX tidak diaktifkan dalam konfigurasi proyek ini
 import LoginScreen from '../screens/auth/LoginScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function AppNavigator() {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen
-        name="Login"
-        component={LoginScreen}
-      />
-    </Stack.Navigator>
+  return React.createElement(
+    Stack.Navigator,
+    null,
+    React.createElement(Stack.Screen, {
+      name: 'Login',
+      component: LoginScreen,
+    })
   );
 }
