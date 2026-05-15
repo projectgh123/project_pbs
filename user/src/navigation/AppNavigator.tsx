053951b108e9a0c4cd3932903065cc8b4f3ab264
SyaifulArief <syaiful_arief@teknokrat.ac.id>
2026-05-15 22:11:15 +0700

    menambah register navigation

--- user/src/navigation/AppNavigator.tsx
+++ user/src/navigation/AppNavigator.tsx
@@ -3,6 +3,8 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 // @ts-ignore: modul yang diimpor menggunakan TSX, tetapi JSX tidak diaktifkan dalam konfigurasi proyek ini
 import LoginScreen from '../screens/auth/LoginScreen';
+// @ts-ignore: modul yang diimpor menggunakan TSX, tetapi JSX tidak diaktifkan dalam konfigurasi proyek ini
+import RegisterScreen from '../screens/auth/RegisterScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,6 +15,10 @@ export default function AppNavigator() {
     React.createElement(Stack.Screen, {
       name: 'Login',
       component: LoginScreen,
+    }),
+    React.createElement(Stack.Screen, {
+      name: 'Register',
+      component: RegisterScreen,
     })
   );
 }
