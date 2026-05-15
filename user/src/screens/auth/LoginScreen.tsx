c99ef8a102c254a0ef1d2f64e4d99bf9aa5b23f9
SyaifulArief <syaiful_arief@teknokrat.ac.id>
2026-05-15 21:34:05 +0700

    Update LoginScreen

--- user/src/screens/auth/LoginScreen.tsx
+++ user/src/screens/auth/LoginScreen.tsx
@@ -1,10 +1,39 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+
+import { globalStyles } from '../../styles/globalStyles';
 
 export default function LoginScreen() {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
   return React.createElement(
     View,
-    null,
-    React.createElement(Text, null, 'Login Screen')
+    { style: globalStyles.container },
+    React.createElement(Text, { style: globalStyles.title }, 'WisataCui'),
+    React.createElement(TextInput, {
+      placeholder: 'Email',
+      style: globalStyles.input,
+      value: email,
+      onChangeText: setEmail,
+    }),
+    React.createElement(TextInput, {
+      placeholder: 'Password',
+      secureTextEntry: true,
+      style: globalStyles.input,
+      value: password,
+      onChangeText: setPassword,
+    }),
+    React.createElement(
+      TouchableOpacity,
+      { style: globalStyles.button },
+      React.createElement(Text, { style: globalStyles.buttonText }, 'Login')
+    )
   );
 }
