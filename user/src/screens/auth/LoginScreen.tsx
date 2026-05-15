9dd322ec985d61334bb8f9bf21404a396bee855b
SyaifulArief <syaiful_arief@teknokrat.ac.id>
2026-05-15 21:23:15 +0700

    Memperbaiki eror LoginScreen

--- user/src/screens/auth/LoginScreen.tsx
+++ user/src/screens/auth/LoginScreen.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { View, Text } from 'react-native';
 
 export default function LoginScreen() {
-  return (
-    <View>
-      <Text>Login Screen</Text>
-    </View>
+  return React.createElement(
+    View,
+    null,
+    React.createElement(Text, null, 'Login Screen')
   );
 }
