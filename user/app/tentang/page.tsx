b8427310815940df4aaa7da1b0b172b0968603de
SyaifulArief <syaiful_arief@teknokrat.ac.id>
2026-05-24 14:16:38 +0700

    menambahkan informasi wisata pada halaman Tentang

---
+++ user/app/tentang/page.tsx
@@ -0,0 +1,30 @@
+export default function Tentang() {
+  return (
+    <main className="min-h-screen p-10 bg-sky-50">
+
+      <h1 className="text-4xl font-bold text-blue-800 mb-6">
+        Tentang WisataCui
+      </h1>
+
+      <p className="text-lg text-gray-700">
+        WisataCui adalah website wisata yang membahas
+        Sanggar Beach di Lampung Selatan, Lampung.
+      </p>
+
+      <div className="mt-6">
+        <p>
+          📍 Lokasi: Lampung Selatan
+        </p>
+
+        <p>
+          🌊 Daya tarik: Pantai, sunset, spot foto
+        </p>
+
+        <p>
+          🎟 Tiket masuk: Rp10.000
+        </p>
+      </div>
+
+    </main>
+  );
+}
