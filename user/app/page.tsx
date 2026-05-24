50c4c7b1b9cf9ba542bd9ead5ce2cbd439ba177a
SyaifulArief <syaiful_arief@teknokrat.ac.id>
2026-05-24 14:11:20 +0700

    memperbarui struktur layout halaman Home

--- user/app/page.tsx
+++ user/app/page.tsx
@@ -5,11 +5,11 @@ import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <>
+    <main className="main-container">
       <Navbar />
       <Hero />
       <CardWisata />
       <Footer />
-    </>
+    </main>
   );
 }
