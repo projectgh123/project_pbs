b4ea9e0cfe15765a59ae2061c2b3362ce673157e
SyaifulArief <syaiful_arief@teknokrat.ac.id>
2026-05-24 14:30:53 +0700

    menambahkan menu navigasi lengkap pada Navbar

--- user/components/Navbar.tsx
+++ user/components/Navbar.tsx
@@ -1,25 +1,24 @@
-import Link from "next/link";
-
 export default function Navbar() {
   return (
-    <nav className="bg-blue-700 text-white p-5">
-      <div className="max-w-6xl mx-auto flex justify-between">
+    <nav className="navbar">
 
-        <h1 className="text-2xl font-bold">
-          🏝 WisataCui
+      <div className="nav-container">
+
+        <h1>
+          🏝️ WisataCui
         </h1>
 
-        <ul className="flex gap-6">
-          <li>
-            <Link href="/">Beranda</Link>
-          </li>
-
-          <li>
-            <Link href="/tentang">Tentang</Link>
-          </li>
+        <ul className="menu">
+          <li>Beranda</li>
+          <li> <a href="/tentang">Tentang</a> </li>
+          <li> <a href="/public">Galeri</a> </li>
+          <li> <a href="/wisata">Wisata</a> </li>
+          <li> <a href="/lokasi">Lokasi</a> </li>
+          <li> <a href="/kontak">Kontak</a> </li>
         </ul>
 
       </div>
+
     </nav>
   );
 }
