e46cf8278c98af3a4006d24ab8bf8078473c5c32
SyaifulArief <syaiful_arief@teknokrat.ac.id>
2026-05-24 13:36:17 +0700

    memisahkan halaman menjadi komponen Navbar Hero dan Footer

--- user/app/page.tsx
+++ user/app/page.tsx
@@ -1,86 +1,13 @@
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import Hero from "@/components/Hero";
+
 export default function Home() {
-
-const wisata = [
-{
-id:1,
-title:"Pantai Sanggar",
-desc:"Pantai indah di Lampung Selatan"
-}
-];
-
-return (
-
-<main className="main-container">
-
-<nav className="navbar">
-<div className="nav-container">
-
-<h1>🏝 WisataCui</h1>
-
-<ul className="menu">
-<li>Beranda</li>
-<li>Tentang</li>
-<li>Galeri</li>
-<li>Wisata</li>
-<li>Lokasi</li>
-<li>Kontak</li>
-</ul>
-
-</div>
-</nav>
-
-<section className="hero">
-
-<h2 className="hero-title">
-Selamat Datang di WisataCui
-</h2>
-
-<p className="hero-text">
-Jelajahi Sanggar Beach Lampung Selatan
-</p>
-
-<button className="button">
-Jelajahi Sekarang
-</button>
-
-</section>
-
-<section className="container">
-
-<h2 className="title">
-Informasi Wisata
-</h2>
-
-<div className="grid">
-
-{wisata.map((item)=>(
-
-<div className="card" key={item.id}>
-
-<div className="image-placeholder">
-Gambar Wisata
-</div>
-
-<div className="card-content">
-
-<h3>{item.title}</h3>
-<p>{item.desc}</p>
-
-</div>
-
-</div>
-
-))}
-
-</div>
-
-</section>
-
-<footer className="footer">
-© 2026 WisataCui
-</footer>
-
-</main>
-
-)
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Footer />
+    </>
+  );
 }
