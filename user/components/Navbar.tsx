2810efb65466ba06ddb63bc9cea9c6d74a5cc2ac
SyaifulArief <syaiful_arief@teknokrat.ac.id>
2026-05-24 14:17:47 +0700

    menambahkan routing navigasi pada Navbar

--- user/components/Navbar.tsx
+++ user/components/Navbar.tsx
@@ -1,17 +1,24 @@
+import Link from "next/link";
+
 export default function Navbar() {
   return (
-    <nav className="navbar">
-      <div className="nav-container">
-        <h1>WisataCui</h1>
+    <nav className="bg-blue-700 text-white p-5">
+      <div className="max-w-6xl mx-auto flex justify-between">
 
-        <ul className="menu">
-          <li>Beranda</li>
-          <li>Tentang</li>
-          <li>Galeri</li>
-          <li>Wisata</li>
-          <li>Lokasi</li>
-          <li>Kontak</li>
+        <h1 className="text-2xl font-bold">
+          🏝 WisataCui
+        </h1>
+
+        <ul className="flex gap-6">
+          <li>
+            <Link href="/">Beranda</Link>
+          </li>
+
+          <li>
+            <Link href="/tentang">Tentang</Link>
+          </li>
         </ul>
+
       </div>
     </nav>
   );
