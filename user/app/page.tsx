8faaddbcfec655b50580eea18ba4ce1bc0b8e289
SyaifulArief <syaiful_arief@teknokrat.ac.id>
2026-05-24 13:43:33 +0700

    menambahkan CardWisata ke halaman utama WisataCui

--- user/app/page.tsx
+++ user/app/page.tsx
@@ -1,12 +1,14 @@
 import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
+import CardWisata from "@/components/CardWisata";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <Hero />
+      <CardWisata />
       <Footer />
     </>
   );
