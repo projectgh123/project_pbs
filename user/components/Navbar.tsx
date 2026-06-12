7dd2d81677c3f2da8f79c90349c19d8176a44a45
SyaifulArief <syaiful_arief@teknokrat.ac.id>
2026-06-12 22:12:07 +0700

    mengganti logo

--- user/components/Navbar.tsx
+++ user/components/Navbar.tsx
@@ -1,27 +1,35 @@
-import Link from "next/link";
+import Image from "next/image";
 
 export default function Navbar() {
   return (
     <nav className="navbar">
-
       <div className="nav-container">
 
-        <h1>
-          🏝️ WisataCui
-        </h1>
+        <div className="logo-area">
+
+          <Image
+            src="/logo1.png"
+            alt="Logo"
+            width={60}
+            height={60}
+          />
+
+          <h1 className="logo">
+          </h1>
+
+        </div>
 
         <ul className="menu">
           <li>Beranda</li>
-          <li> <a href="/tentang">Tentang</a> </li>
-          <li> <Link href="/galeri">Galeri</Link> </li>
-          <li> <a href="/wisata">Wisata</a> </li>
-          <li> <a href="/lokasi">Lokasi</a> </li>
-          <li> <a href="/kontak">Kontak</a> </li>
+          <li><a href="/tentang">Tentang</a></li>
+          <li><a href="/galeri">Galeri</a></li>
+          <li><a href="/wisata">Wisata</a></li>
+          <li><a href="/lokasi">Lokasi</a></li>
+          <li><a href="/kontak">Kontak</a></li>
           <li><a href="/panduan">Panduan</a></li>
         </ul>
 
       </div>
-
     </nav>
   );
 }
