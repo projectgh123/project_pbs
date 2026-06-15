e7d3f9d9cecdc289914c7bfdf799852827c7d8e9
projectgh123 <muhammad_aira@teknokrat.ac.id>
2026-06-15 21:53:52 +0700

    update auth service

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,14 +1,12 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import { Injectable } from '@nestjs/common';
 import { LoginDto } from './dto/login.dto';
 
-@Controller('auth')
-export class AuthController {
-
-  constructor(private authService: AuthService) {}
-
-  @Post('login')
-  login(@Body() body: LoginDto) {
-    return this.authService.login(body);
+@Injectable()
+export class AuthService {
+  async login(body: LoginDto) {
+    return {
+      message: 'Login berhasil',
+      user: body.username,
+    };
   }
 }
