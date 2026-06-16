47653d6e9f6d9581a76ef4d6f4db5e47323fb3dc
projectgh123 <muhammad_aira@teknokrat.ac.id>
2026-06-17 00:50:07 +0700

    category service

--- backend/src/category/category.service.ts
+++ backend/src/category/category.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
 
 @Injectable()
 export class CategoryService {
