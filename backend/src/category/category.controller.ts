d0a7aaa51171e7126fc5882b43308cb95dc676a6
projectgh123 <muhammad_aira@teknokrat.ac.id>
2026-06-17 00:29:35 +0700

    category controll

--- backend/src/category/category.controller.ts
+++ backend/src/category/category.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CategoryService } from './category.service';
 
 @Controller('category')
