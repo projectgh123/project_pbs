9338be09ead8b92822b08d1d56188e13971a4a0d
projectgh123 <muhammad_aira@teknokrat.ac.id>
2026-06-17 00:29:57 +0700

    category controll

--- backend/src/category/category.controller.ts
+++ backend/src/category/category.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CategoryService } from './category.service';
+import { CreateCategoryDto } from './dto/create-category.dto';
 
 @Controller('category')
 export class CategoryController {
